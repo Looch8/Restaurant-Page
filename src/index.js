12ad9595d1e72c46b6dee3b3e005ddbd127f9bdd
Luke Wheldale <lukewheldale@gmail.com>
2024-03-15 15:29:38 +1030

    html added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log("test");
+
+// import logo from "/logo.png";
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
 	},
+	//  Images
+	module: {
+		rules: [{ test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" }],
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "src/index.html",
