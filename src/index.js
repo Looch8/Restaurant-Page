29cce10b0086c62c92ba9caee0f9a86f523bed99
Luke Wheldale <lukewheldale@gmail.com>
2024-03-16 12:08:05 +1030

    bug fix: fixed undefined being rendered. was trying to append component function without returning anything

--- src/index.js
+++ src/index.js
@@ -24,10 +24,4 @@ function component() {
 	});
 }
 
-document.body.append(component());
-
-// Next, set up your restaurant site to use tabbed browsing to access the Contact and Menu pages. Look at the behavior of this student’s live preview site for visual inspiration.
-
-// Put the contents of each ‘tab’ inside of its own module. Each module will export a function that creates a div element, adds the appropriate content and styles to that element and then appends it to the DOM.
-
-// Write the tab-switching logic inside of index.js. You should have event listeners for each button in the header navbar that wipes out the current contents of div#content and then runs the correct ‘tab module’ to populate it with the new contents again.
+component();
